1803071c057b12d2bf8dfe75268c966d7ce75427
Vishal-1008 <itsvishal.dev@gmail.com>
2026-03-03 19:43:00 +0530

    upgrade landing page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component} from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AboutComponent } from "../about/about.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterLinkActive, RouterLink],
+  imports: [CommonModule, RouterLinkActive, RouterLink, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  currentYear: number = new Date().getFullYear();
 }
